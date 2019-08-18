5b7d291c452af7870397d214a59fdf50d6c8ba5a
Andrew Williams <1williamsaj@gmail.com>
2019-08-18 14:06:14 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,19 +1,19 @@
 # Use this space to code along with the readme
 
-#phrase = "Hello World!"
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
-#puts phrase
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
 
 def greeting
   puts "Hello World!"
 end
 
-greeting 
+greeting
