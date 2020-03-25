764ee21df20ccb091d60c3902a1d650d3b0b86c7
Lesly Juarez <leslyj2@gmail.com>
2020-03-25 23:06:16 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,7 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
+def say_hello_world_ten_times 
+  phrase = "Hello World!"
 puts phrase
 puts phrase
 puts phrase
@@ -12,3 +13,5 @@ puts phrase
 puts phrase
 puts phrase
 end
+
+say_hello_world_ten_times
