62a49f493495572b21d4ab77f33965f14777ca06
immutable-integer-8885 <gidelbusto@gmail.com>
2020-04-05 03:46:19 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,16 @@
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+def say_hello_ten_times
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
