07e818eae917ebdf2c347196a5e0921c3560b372
Alvin Coulson <aaacoulson@gmail.com>
2020-05-22 01:40:50 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,15 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
