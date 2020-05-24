e4ccfe5c76cfeab342c81af430f4833993d13b0b
Ryan Pearce <ryan.pearce@key.me>
2020-05-24 15:51:35 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
+
   phrase = "Hello World!"
   puts phrase
   puts phrase
@@ -11,10 +11,6 @@ def say_hello_world_ten_times
   puts phrase
   puts phrase
   puts phrase
-end
 
-def greeting
-  puts "Hello World"
-end
 
-greeting
+
