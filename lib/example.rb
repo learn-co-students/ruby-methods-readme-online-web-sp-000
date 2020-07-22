f99efb87d7b722ab3318d65961b705fa210990bd
Jason Keeling <s2jason@yahoo.com>
2020-07-22 23:43:49 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,18 +1,10 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting
+  puts "Hello World"
 end
-
-def greeting # Method Signature
-  puts "Hello World" # Method Body
-end # Method Closing
+ 
+greeting
+greeting
+greeting
+greeting
+greeting
