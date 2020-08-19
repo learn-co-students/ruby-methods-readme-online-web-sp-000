6c3c1f3e4bda3f5f13b78dd0a94021d5dfbd094f
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-19 01:34:50 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,13 @@
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+def greeting
+  puts phrase
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
