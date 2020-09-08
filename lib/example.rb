30e5e761a696a3a33c2dde20cc3ec3cfde5eba88
Chandra Lama <iamme24cl@gmail.com>
2020-09-08 06:26:16 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,13 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+
+
+def print_ten_times
+  phrase = "Hello World!\n"
+  output = phrase * 10
+  puts output
+end
+
+print_ten_times
+
