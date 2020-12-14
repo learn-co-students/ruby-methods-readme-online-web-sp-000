251d5c50df0bba550967855cf3ade380d48640a3
github username <github email address>
2020-12-14 22:56:15 +0200

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-
+def say_hello_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +11,9 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+
+
+def greeting 
+    puts "Hello World"
+end
