beccdab43456604db72a0507179e3612d73c98c1
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-02-11 09:27:11 -0500

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -15,15 +15,17 @@ puts phrase
 def say_hello_ten_times
   
   phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 
 end
+
+
