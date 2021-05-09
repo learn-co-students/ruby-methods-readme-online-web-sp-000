9c7e23789448c9c2caf4a3e38729c12782562003
Ian King <kickthechair@gmail.com>
2021-05-09 14:07:27 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,12 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+
+#Abstraction of having to puts the phrase 10 time_select through the use of a method we can then call to do execut the same thing. This has additionally been abstracted further through the use of a loop.
+
+def say_hello_world_ten_times
+  10.times do
+    puts "Hello World"
+  end
+end
