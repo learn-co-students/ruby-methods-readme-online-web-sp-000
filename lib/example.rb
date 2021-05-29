f75044cd8ca9bb439509930b6acc4cb445d4d12e
Alishan  Prasala <alishanp86@gmail.com>
2021-05-29 16:13:55 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,16 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
